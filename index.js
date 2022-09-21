5d8c7c6d616538f75a64da4fd691062be40ff69f
Yuly Marcela Velasquez <marcela.velasquez@netuxtecnologia.com>
2022-09-21 11:46:25 -0500

    feat: add fastify configuration

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// Import modules
 import { config } from './config/environment.js'
+import fastify from 'fastify'
 
-console.log(config)
+const { port } = config
+
+// Configure fastify server
+const server = fastify({
+  logger: { level: 'info' },
+})
+
+server.get('/ok', (request, reply) => {
+  reply.status(204).send()
+})
+
+// Start fastify server
+try {
+  await server.listen({ port })
+} catch (err) {
+  server.log.error(err)
+  process.exit(1)
+}
