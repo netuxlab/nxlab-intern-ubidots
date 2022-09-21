e3bff0cdfbe522de0558d313ad5663f1379f1166
Yuly Marcela Velasquez <marcela.velasquez@netuxtecnologia.com>
2022-09-21 11:00:38 -0500

    feat: add base configuration

---
+++ config/environment.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: `${__dirname}/../.env` })
+const config = {
+  port: process.env.PORT ?? 8000,
+}
+
+export { config }
---
+++ index.js
@@ -0,0 +1,3 @@
+import { config } from './config/environment.js'
+
+console.log(config)
