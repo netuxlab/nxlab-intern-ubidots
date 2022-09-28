861a9fa611f28682342376bca28289757e772ee9
davidsierradz <davidsierradz@gmail.com>
2022-09-28 11:13:24 -0500

    feat: add endpoint to get system's date

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
+import { execSync } from 'child_process'
+
 const generateRoutes = (server) => {
   server.get('/ok', (request, reply) => {
     reply.status(204).send()
+  })
+
+  server.get('/date', (request, reply) => {
+    const dateString = execSync('date', {
+      encoding: 'utf-8',
+      ...(process.platform === 'win32' ? { shell: 'C:\\Program Files\\Git\\bin\\bash.exe' } : {}),
+    })
+
+    reply.send({ data: dateString.slice(0, -1) })
   })
 }
 
