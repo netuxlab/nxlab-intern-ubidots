d1c84b141e63472fd66bed6b644c6562bc9e9ff8
alejandro0717 <alejandro.vergara@netuxtecnologia.com>
2022-10-26 09:57:11 -0500

    fix: support windows path

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import {join} from 'path'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-dotenv.config({ path: `${__dirname}/../.env` })
+dotenv.config({ path: join(__dirname,'/../.env') })
 const config = {
-  port: process.env.PORT ?? 8000,
+  port: process.env.PORT ?? 7000,
+  ubidotsToken: process.env.UBIDOTS_TOKEN,
 }
 
 export { config }
